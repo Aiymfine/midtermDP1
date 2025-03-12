9e7773489c4ac405453f52eb3a4c412b1f027c35
aiymfine <147755224+Aiymfine@users.noreply.github.com>
2025-03-13 00:37:47 +0500

    such a headache, here it is

---
+++ onlinePaymentMidtermTask/CreditCardConfirmer.java
@@ -0,0 +1,23 @@
+class CreditCardConfirmer{
+    public boolean confirmCard(String cardNum){
+        return cardNum.length()==16;
+    } //mock API for Credit Card validation
+}//checks if credit number is correct and contain 16 digits
+
+class CreditCardPAdapter implements PaymentMethod{
+     CreditCardPayment creditCardPayment;
+     CreditCardConfirmer confirmer;
+     String cardNum;
+
+    public CreditCardPAdapter(String cardNum){
+        this.creditCardPayment= new CreditCardPayment();
+        this.confirmer= new CreditCardConfirmer();
+        this.cardNum= cardNum;
+    }
+    public void processPayment(double amount){
+        if(!confirmer.confirmCard(cardNum)){
+          throw new IllegalArgumentException("WRONG CREDIT CARD NUMBER!");}     //if the digits of credit card is not 16, it throws exception
+          creditCardPayment.processPayment(amount);
+          System.out.println("succesful payment!");
+}}// heer we use adapter pattern, its like a bridge,
+// makes sure that payment system can work with credit card validation system
